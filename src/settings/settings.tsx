47bcc3e125e7418820d0397d9a649dac1a1c8079
Araz <araz944memmedov@gmail.com>
2022-11-07 19:51:18 +0400

    setting.tsx

---
+++ src/settings/settings.tsx
@@ -0,0 +1,12 @@
+export const type = [
+  { id: 1, name: "Range" },
+  { id: 2, name: "Single" },
+];
+
+export const format = [
+  { id: 1, name: "Currency", description: "currency" },
+  { id: 2, name: "Percent", description: "percent" },
+  { id: 3, name: "Number", description: "number" },
+  { id: 4, name: "Text", description: "text" },
+  { id: 5, name: "Date", description: "date" },
+];
